0a28d98efdee25e5f7a39d5fe7b455d59d9980bd
Hugo <hugo.djemaa@gmail.com>
2017-01-11 14:38:46 -0200

    auto_send funcional

--- android/PiPong/app/src/main/java/com/realsoc/pipong/GameActivity.java
+++ android/PiPong/app/src/main/java/com/realsoc/pipong/GameActivity.java
@@ -1,5 +1,6 @@
 package com.realsoc.pipong;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,7 +33,11 @@ import static com.realsoc.pipong.Constants.REMOTE_SERVER_ADDRESS;
  */
 
 public class GameActivity extends AppCompatActivity{
-
+    private Dialog sendDialog;
+    private int countdownSendData =5;
+    private TextView autoSendMessage;
+    private boolean autoSend = true;
+    private boolean exit = false;
     private String player1;
     private String player2;
     private int type;
@@ -68,6 +74,7 @@ public class GameActivity extends AppCompatActivity{
         scoreP2TextView.setText(Integer.toString(scoreP2));
         ((TextView) findViewById(R.id.player1_name)).setText(player1);
         ((TextView) findViewById(R.id.player2_name)).setText(player2);
+        //sendAndShowDialog();
     }
 
     private void updateServeShow() {
@@ -109,14 +116,93 @@ public class GameActivity extends AppCompatActivity{
             }
         }
     }
+    private void testEndGame() {
+        if(autoSend == true){
+            if((scoreP1 >= type && scoreP1>scoreP2+1)||(scoreP2>=type && scoreP2>scoreP1+1)){
+                sendAndShowDialog();
+            }
+        }
+    }
+
+    private void sendAndShowDialog() {
+        final int nextType = (type == 6) ? 11: 21;
+        String passToNext = "Continue to "+Integer.toString(nextType);
+        String test = "Hello";
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View dialogView = factory.inflate(R.layout.autosend_dialog, null);
+        autoSendMessage=(TextView)dialogView.findViewById(R.id.autosend_dialog_message);
+        //alert.setMessage(test);
+        alert.setView(dialogView);
+        alert.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                exit = true;
+                dialog.cancel();
+            }
+        });
+        if(type == 6 ||type == 11){
+            alert.setPositiveButton(passToNext,
+                    new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            exit = true;
+                            type = nextType;
+                            dialog.cancel();
+                        }
+                    });
+        }
+
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        autoSend = false;
+                        dialog.cancel();
+                    }
+                });
+        sendDialog = alert.create();
+        sendDialog.show();
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (autoSend&&!exit) {
+                        Log.d("THERE","BITCH + "+countdownSendData);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(countdownSendData == 0){
+                                    autoSend = false;
+                                    sendData(null);
+                                }else{
+                                    //alert.setMessage("Sending datas in ("+countdownSendData+")...");
+                                    autoSendMessage.setText("Sending datas in ("+countdownSendData+")...");
+                                    countdownSendData--;
+                                }
+                            }
+                        });
+                        Thread.sleep(1000);
+                    }
+                    countdownSendData = 5;
+                    exit = false;
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+    }
 
     public void player1Plus(View v){
         scoreP1++;
+        testEndGame();
         updateServeShow();
         scoreP1TextView.setText(Integer.toString(scoreP1));
     }
+
     public void player2Plus(View v){
         scoreP2++;
+        testEndGame();
         updateServeShow();
         scoreP2TextView.setText(Integer.toString(scoreP2));
     }
@@ -134,36 +220,29 @@ public class GameActivity extends AppCompatActivity{
             scoreP2TextView.setText(Integer.toString(scoreP2));
         }
     }
-    public void sendData(View v){
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(GameActivity.this);
-        alertDialog.setTitle("Confirm");
-        alertDialog.setMessage("You really want to persist datas");
+    public void sendToServer(){
+        String req = "{\"player1\":\""+player1+"\",\"player2\":\""+player2+"\",\"sPlayer1\":"+scoreP1+",\"sPlayer2\":"+scoreP2+",\"type\":"+type+"}";
+        Log.d(PLAYERS_ACTIVITY_NAME,req);
+        RequestBody body = RequestBody.create(JSON,req);
+        Request request = new Request.Builder()
+                .url(REMOTE_SERVER_ADDRESS+"/api/games")
+                .post(body)
+                .build();
+        Log.d(PLAYERS_ACTIVITY_NAME,body.toString());
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Failed to persist",Toast.LENGTH_LONG).show();
+            }
 
-        alertDialog.setPositiveButton("Send",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        String req = "{\"player1\":\""+player1+"\",\"player2\":\""+player2+"\",\"sPlayer1\":"+scoreP1+",\"sPlayer2\":"+scoreP2+",\"type\":"+type+"}";
-                        Log.d(PLAYERS_ACTIVITY_NAME,req);
-                        RequestBody body = RequestBody.create(JSON,req);
-                        Request request = new Request.Builder()
-                                .url(REMOTE_SERVER_ADDRESS+"/api/games")
-                                .post(body)
-                                .build();
-                        Log.d(PLAYERS_ACTIVITY_NAME,body.toString());
-                        Call call = client.newCall(request);
-                        call.enqueue(new Callback() {
-                            @Override
-                            public void onFailure(Call call, IOException e) {
-                                e.printStackTrace();
-                                Toast.makeText(getApplicationContext(),"Failed to persist",Toast.LENGTH_LONG).show();
-                            }
-
-                            @Override
-                            public void onResponse(Call call, final Response response) throws IOException {
-                                Log.d(GAME_ACTIVITY_NAME,response.body().string());
-                                Intent intent = new Intent ( getApplicationContext() , MainActivity.class );
-                                intent.addFlags ( Intent.FLAG_ACTIVITY_CLEAR_TOP );
-                                startActivity ( intent );
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                Log.d(GAME_ACTIVITY_NAME,response.body().string());
+                Intent intent = new Intent ( getApplicationContext() , MainActivity.class );
+                intent.addFlags ( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+                startActivity ( intent );
                                     /*new Handler(Looper.getMainLooper()).post(new Runnable() {
                                         @Override
                                         public void run() {
@@ -174,10 +253,18 @@ public class GameActivity extends AppCompatActivity{
                                             }
                                         }
                                     });*/
-                            }
-                        });
+            }
+        });
+    }
+    public void sendData(View v){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(GameActivity.this);
+        alertDialog.setTitle("Confirm");
+        alertDialog.setMessage("You really want to persist datas");
 
-
+        alertDialog.setPositiveButton("Send",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                       sendToServer();
                     }
                 });
 
@@ -187,9 +274,21 @@ public class GameActivity extends AppCompatActivity{
                         dialog.cancel();
                     }
                 });
-
-        alertDialog.show();
+        if(v == null){
+            sendToServer();
+        }else{
+            alertDialog.show();
+        }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sendDialog != null){
+            sendDialog.dismiss();
+        }
+    }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
 
